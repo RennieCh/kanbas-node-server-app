04809faeaf71af332ccd8ba6fa1a7756502d428a
RennieCh <119545126+RennieCh@users.noreply.github.com>
2024-11-24 17:38:59 -0500

    a6 first commit

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
+import "dotenv/config";
 import express from "express";
+import mongoose from "mongoose";
 import "dotenv/config";
 import session from "express-session";
 import Hello from "./Hello.js";
@@ -9,6 +11,9 @@ import CourseRoutes from "./Kanbas/Courses/routes.js";
 import ModuleRoutes from "./Kanbas/Modules/routes.js";
 import AssignmentRoutes from "./Kanbas/Assignments/routes.js";
 import EnrollmentRoutes from "./Kanbas/Enrollments/routes.js";
+
+const CONNECTION_STRING = process.env.MONGO_CONNECTION_STRING || "mongodb://127.0.0.1:27017/kanbas-cs5610-fa24"
+mongoose.connect(CONNECTION_STRING);
 
 const app = express();
 app.use(cors({
